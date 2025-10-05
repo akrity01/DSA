900b6f39d5dd0c52dbe14441d722e7b742a36bc9
Akriti Rawat <akritirawat424@gmail.com>
2025-10-05 17:14:25 +0530

    Time: 22 ms (28.51%) | Memory: 58.8 MB (18.86%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,35 +1,41 @@
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if ((long) m * k > n) return -1;
-
-        int left = 1, right = 1_000_000_000;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canMakeBouquets(bloomDay, m, k, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
-    private boolean canMakeBouquets(int[] bloomDay, int m, int k, int days) {
-        int bouquets = 0, flowers = 0;
+    // Helper function to check if we can make m bouquets in 'mid' days
+    private boolean possible(int[] bloomDay, int mid, int m, int k) {
+        int cnt = 0;
+        int bouquets = 0;
 
         for (int bloom : bloomDay) {
-            if (bloom <= days) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
+            if (bloom <= mid) {
+                cnt++;
                 }
-            } else {
-                flowers = 0; // reset because not adjacent
-            }
+                else {
+                     bouquets +=(cnt/ k);
+                     cnt= 0 ;
+                     }
+                     }
+                bouquets += ( cnt/k);
+        return bouquets >= m;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        long totalFlowers = (long) m * k;
+        if (totalFlowers > bloomDay.length) return -1; // impossible
+
+        int minDay = Integer.MAX_VALUE, maxDay = Integer.MIN_VALUE;
+        for (int bloom : bloomDay) {
+            minDay = Math.min(minDay, bloom);
+            maxDay = Math.max(maxDay, bloom);
         }
 
-        return bouquets >= m;
+        int low = minDay, high = maxDay;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1; // try earlier day
+            } else {
+                low = mid + 1;  // need more days
+            }
+        }
+        return low;
     }
 }
