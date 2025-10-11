f562412a62fa01a4af92dec4da255df3ffc15839
Akriti Rawat <akritirawat424@gmail.com>
2025-10-11 10:00:01 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (47.75%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,26 +2,30 @@ class Solution {
     public int myAtoi(String s) {
         if (s == null) return 0;
 
-        s = s.trim();
-        
-        if (s.length() == 0) return 0;
-        
-        int sign = +1;
-        long ans = 0;
-        if (s.charAt(0) == '-') sign = -1;
-        
-        int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
-		
-        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
-		
-        while (i < s.length()) {
-            if (s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i) - '0';
-            if (sign == -1 && -1 * ans < MIN) return MIN;
-            if (sign == 1 && ans > MAX) return MAX;
+        int n = s.length(), i = 0, sign = 1, result = 0;
+
+        // 1. Skip leading whitespaces
+        while (i < n && s.charAt(i) == ' ') i++;
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
-        
-        return (int)(sign * ans);
+
+        // 3. Convert digits to integer
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            // 4. Check for overflow before multiplying
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
     }
 }
