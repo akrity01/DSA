fa1f15a2c20fa433d8af7f464568bab5786ed319
Akriti Rawat <akritirawat424@gmail.com>
2026-01-09 22:48:18 +0530

    Time: 8 ms (92.72%) | Memory: 73.7 MB (5.47%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,16 +1,46 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int left = 0, max = 0;
+        int type1 = -1, type2 = -1;   // two basket types
+        int count1 = 0, count2 = 0;  // counts of those types
+        int max = 0;
+
+        int left = 0;
 
         for (int right = 0; right < fruits.length; right++) {
-            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+            int fruit = fruits[right];
 
-            while (map.size() > 2) {
-                map.put(fruits[left], map.get(fruits[left]) - 1);
-                if (map.get(fruits[left]) == 0)
-                    map.remove(fruits[left]);
-                left++;
+            // If fruit matches one of the basket types
+            if (fruit == type1) {
+                count1++;
+            } 
+            else if (fruit == type2) {
+                count2++;
+            } 
+            else if (type1 == -1) {
+                type1 = fruit;
+                count1 = 1;
+            } 
+            else if (type2 == -1) {
+                type2 = fruit;
+                count2 = 1;
+            } 
+            else {
+                // Third type found → shrink window
+                while (count1 > 0 && count2 > 0) {
+                    int leftFruit = fruits[left];
+                    if (leftFruit == type1) count1--;
+                    else count2--;
+                    left++;
+                }
+
+                // One basket is now empty → replace it
+                if (count1 == 0) {
+                    type1 = fruit;
+                    count1 = 1;
+                } else {
+                    type2 = fruit;
+                    count2 = 1;
+                }
             }
 
             max = Math.max(max, right - left + 1);
