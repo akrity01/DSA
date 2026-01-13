336eedd4d8b8a3d1204bb1eb7a4db5a098421db5
Akriti Rawat <akritirawat424@gmail.com>
2026-01-13 22:06:21 +0530

    Time: 2 ms (99.68%) | Memory: 45.7 MB (74.41%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,33 +1,29 @@
-import java.util.HashMap;
-
 class Solution {
     public String minWindow(String s, String t) {
 
         if (t.length() > s.length()) return "";
 
-        HashMap<Character, Integer> need = new HashMap<>();
+        int[] need = new int[128];   // ASCII characters
         for (char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
+            need[c]++;
         }
-
-        HashMap<Character, Integer> window = new HashMap<>();
-        int have = 0;
-        int needCount = need.size();
 
         int left = 0;
         int minLen = Integer.MAX_VALUE;
         int start = 0;
+        int required = t.length();
 
         for (int right = 0; right < s.length(); right++) {
 
             char c = s.charAt(right);
-            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            if (need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()) {
-                have++;
+            if (need[c] > 0) {
+                required--;
             }
+            need[c]--;
 
-            while (have == needCount) {
+            // Window valid when required == 0
+            while (required == 0) {
 
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
@@ -35,10 +31,10 @@ class Solution {
                 }
 
                 char leftChar = s.charAt(left);
-                window.put(leftChar, window.get(leftChar) - 1);
+                need[leftChar]++;
 
-                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
-                    have--;
+                if (need[leftChar] > 0) {
+                    required++;
                 }
 
                 left++;
