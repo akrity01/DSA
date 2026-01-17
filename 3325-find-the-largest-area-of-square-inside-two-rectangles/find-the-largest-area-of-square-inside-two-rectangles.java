b2d2d50b249e40cfc9be37bce6cba59469431c40
Akriti Rawat <akritirawat424@gmail.com>
2026-01-17 23:26:01 +0530

    Time: 26 ms (87.50%) | Memory: 47.9 MB (27.08%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,40 +1,23 @@
 class Solution {
-    public long largestSquareArea(int[][] bl, int[][] tr) {
-        int n = bl.length;
 
-        Integer[] idx = new Integer[n];
-        for (int i = 0; i < n; i++) idx[i] = i;
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length;
+        long maxSide = 0;
 
-        // Sort by left x
-        Arrays.sort(idx, (i, j) -> Integer.compare(bl[i][0], bl[j][0]));
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int w =
+                    Math.min(topRight[i][0], topRight[j][0]) -
+                    Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int h =
+                    Math.min(topRight[i][1], topRight[j][1]) -
+                    Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int side = Math.min(w, h);
 
-        long best = 0;
-
-        for (int a = 0; a < n; a++) {
-            int i = idx[a];
-
-            int ax1 = bl[i][0], ay1 = bl[i][1];
-            int ax2 = tr[i][0], ay2 = tr[i][1];
-
-            // Only check rectangles whose left is before ax2
-            for (int b = a + 1; b < n && bl[idx[b]][0] < ax2; b++) {
-
-                int j = idx[b];
-
-                // fast Y rejection
-                if (tr[j][1] <= ay1 || bl[j][1] >= ay2)
-                    continue;
-
-                int xOverlap = Math.min(ax2, tr[j][0]) - bl[j][0];
-                int yOverlap = Math.min(ay2, tr[j][1]) - Math.max(ay1, bl[j][1]);
-
-                int side = Math.min(xOverlap, yOverlap);
-
-                if (side > best)
-                    best = side;
+                maxSide = Math.max(maxSide, side);
             }
         }
 
-        return best * best;
+        return maxSide * maxSide;
     }
 }
