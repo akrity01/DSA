d1c5477435c66020d193649593adc5b653628851
Akriti Rawat <akritirawat424@gmail.com>
2026-02-03 07:52:07 +0530

    Time: 5 ms (74.42%) | Memory: 46.1 MB (47.55%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,37 +1,37 @@
-// class Solution {
-//     public boolean isPalindrome(int x) {
-//         if ( x<0|| x%10 ==0 && x!= 0 )
-//         {
-//             return false;
-//         }
-//          int revHalf= 0 ;
-//          while( revHalf<x )
-//          {
-// int rem = x%10;
-// revHalf = revHalf *10+rem;
-//  x=x/10; 
-//          }
-//           return (revHalf==x|| x==revHalf/10);
-//     }
-// }
 class Solution {
     public boolean isPalindrome(int x) {
-
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        if ( x<0|| x%10 ==0 && x!= 0 )
+        {
             return false;
         }
-
-        int rev = 0;
-
-        while (x > rev) {
-            int digit = x % 10;
-            rev = rev * 10 + digit;
-            x = x / 10;
-        }
-
-        return (x == rev || x == rev / 10);
+         int revHalf= 0 ;
+         while( x>revHalf )
+         {
+int rem = x%10;
+revHalf = revHalf*10+ rem;
+ x=x/10; 
+         }
+          return (x==revHalf|| x==revHalf/10);
     }
 }
+// class Solution {
+//     public boolean isPalindrome(int x) {
+
+//         if (x < 0 || (x % 10 == 0 && x != 0)) {
+//             return false;
+//         }
+
+//         int rev = 0;
+
+//         while (x > rev) {
+//             int digit = x % 10;
+//             rev = rev * 10 + digit;
+//             x = x / 10;
+//         }
+
+//         return (x == rev || x == rev / 10);
+//     }
+// }
 
 
 
