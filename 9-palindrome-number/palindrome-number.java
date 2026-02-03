edb981925e5c17f676eeba9d7d3a7f00edf9f600
Akriti Rawat <akritirawat424@gmail.com>
2026-02-03 07:43:01 +0530

    Time: 4 ms (99.64%) | Memory: 45.9 MB (67.80%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if ( x<0|| x%10 ==0 && x!= 0 )
+        {
+            return false;
+        }
+         int revHalf= 0 ;
+         while( revHalf<x )
+         {
+int rem = x%10;
+revHalf = revHalf *10+rem;
+ x=x/10; 
+         }
+          return (revHalf==x || x==revHalf/10);
+    }
+}
