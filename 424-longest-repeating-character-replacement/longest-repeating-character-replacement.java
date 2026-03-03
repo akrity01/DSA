fdf42f1e8fdabeb9aa3e8b0dec94511b2553b898
Akriti Rawat <akritirawat424@gmail.com>
2026-03-04 00:18:41 +0530

    Time: 94 ms (5.12%) | Memory: 46.5 MB (31.70%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,28 +1,36 @@
 class Solution {
-    public int characterReplacement(String s, int k) {
-
-        int[] freq = new int[26];  // frequency of letters A-Z
-        int left = 0;
-        int maxFreq = 0;
-        int maxLen = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-
-            char ch = s.charAt(right);
-            freq[ch - 'A']++;
-
-            // Track most frequent character in window
-            maxFreq = Math.max(maxFreq, freq[ch - 'A']);
-
-            // If more than k replacements needed → shrink window
-            while ((right - left + 1) - maxFreq > k) {
-                freq[s.charAt(left) - 'A']--;
-                left++;
-            }
-
-            maxLen = Math.max(maxLen, right - left + 1);
+    public int find(int [] freq){
+        int maxi= - 1;
+        for ( int i = 0 ; i<256; i++)
+        {
+            maxi= Math.max(maxi, freq[i]);
         }
+        return maxi;
+    } 
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+        int f[]= new int[256];
+        int low=0;
+        int high=0;
+        int res= Integer.MIN_VALUE;
+        for ( high=0 ; high< n ; high++)
+        {
+            f[s.charAt(high)]++;
+            int len = high- low +1;
+            int maxc= find(f);
+            int diff= len-maxc; 
 
-        return maxLen;
+            while(diff>k)
+            {
+                f[s.charAt(low)]--;
+                low++;
+                len = high- low +1;
+                maxc= find(f);
+                diff= len-maxc; 
+            }
+        len= high- low +1 ;
+        res= Math.max(res, len);
+        }
+    return res;
     }
 }
