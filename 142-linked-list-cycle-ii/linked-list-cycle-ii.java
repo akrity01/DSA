424879fabed5c886aaa89ae205598cf7cbf1036e
Akriti Rawat <akritirawat424@gmail.com>
2026-03-06 01:01:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (27.64%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,34 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-
-        if(head == null || head.next == null)
-            return null;
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        // Step 1: Detect cycle
-        while(fast != null && fast.next != null){
-
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if(slow == fast)
-                break;
+     if (head== null) return null ;
+     ListNode slow = head;
+     ListNode fast= head;
+      while ( fast !=null && fast.next !=null )
+      {
+        slow= slow.next;
+        fast= fast.next.next;
+        if ( slow==fast)
+        {
+         ListNode p  = head;
+         while( p !=slow)
+         { p = p.next;
+          slow=slow.next;
+         }
+         return slow;
         }
-
-        // No cycle
-        if(fast == null || fast.next == null)
-            return null;
-
-        // Step 2: Find start of cycle
-        slow = head;
-
-        while(slow != fast){
-            slow = slow.next;
-            fast = fast.next;
-        }
-
-        return slow;
-    }
+      }
+        return null;
+    } 
 }
