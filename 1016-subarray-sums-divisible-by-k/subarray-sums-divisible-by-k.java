ef0a55dd2c68dd10b151689936998f490b7438f3
Akriti Rawat <akritirawat424@gmail.com>
2026-04-07 05:28:53 +0530

    Time: 3 ms (99.87%) | Memory: 49 MB (93.51%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,34 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // Base case: remainder 0 appears once
-        map.put(0, 1);
-        
-        int prefixSum = 0;
-        int count = 0;
-        
+        int[] count = new int[k];
+        count[0] = 1;  // base case
+
+        int sum = 0, res = 0;
+
         for (int num : nums) {
-            prefixSum += num;
-            
-            int remainder = prefixSum % k;
-            
-            // Handle negative remainder
-            if (remainder < 0) {
-                remainder += k;
-            }
-            
-            // If remainder seen before, add its frequency
-            if (map.containsKey(remainder)) {
-                count += map.get(remainder);
-            }
-            
-            // Update frequency
-            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+            sum += num;
+
+            int rem = sum % k;
+            if (rem < 0) rem += k;
+
+            res += count[rem]; // add previous occurrences
+
+            count[rem]++; // update frequency
         }
-        
-        return count;
+
+        return res;
     }
 }
